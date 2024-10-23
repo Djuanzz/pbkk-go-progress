44f7e63e9f0d57747bb4c6bc93f6ba53dea47a28
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2024-10-23 16:08:23 +0700

    feat: array and slice

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ func main() {
 	conferenceName := "PBKK"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
+	bookings := []string{}
 
 	fmt.Printf("Welcome to %v bookin system\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are tickets available\n", conferenceTickets, remainingTickets)
@@ -30,5 +31,14 @@ func main() {
 	fmt.Scanln(&userTickets)
 
 	fmt.Printf("Thank you %v %v for booking %v tickets for %v. You will receive a convirmation email at %v\n", firstName, lastName, userTickets, conferenceName, email)
+
+	remainingTickets -= userTickets
+	fmt.Printf("There are %v tickets remaining\n", remainingTickets)
+
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("Data type : %T\n", bookings)
+	fmt.Printf("Data : %v\n", bookings)
+	fmt.Printf("Data length : %v\n", len(bookings))
 
 }
