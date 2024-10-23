a167e12ddae64b2f7c85a3654d66a0067fd93e57
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2024-10-23 17:30:11 +0700

    feat: for and conditional

--- main.go
+++ main.go
@@ -18,27 +18,54 @@ func main() {
 	var email string
 	var userTickets uint
 
-	fmt.Println("Please enter your first name")
-	fmt.Scanln(&firstName)
+	// --- LOOPING
+	for {
+		fmt.Println("Please enter your first name")
+		fmt.Scanln(&firstName)
 
-	fmt.Println("Please enter your last name")
-	fmt.Scanln(&lastName)
+		fmt.Println("Please enter your last name")
+		fmt.Scanln(&lastName)
 
-	fmt.Println("Please enter your email")
-	fmt.Scanln(&email)
+		fmt.Println("Please enter your email")
+		fmt.Scanln(&email)
 
-	fmt.Println("Please enter the number of tickets you want to book")
-	fmt.Scanln(&userTickets)
+		fmt.Println("Please enter the number of tickets you want to book")
+		fmt.Scanln(&userTickets)
 
-	fmt.Printf("Thank you %v %v for booking %v tickets for %v. You will receive a convirmation email at %v\n", firstName, lastName, userTickets, conferenceName, email)
+		if userTickets > remainingTickets {
+			fmt.Printf("Invalid input cuz there are only %v tickets\n", remainingTickets)
+			continue
+		}
 
-	remainingTickets -= userTickets
-	fmt.Printf("There are %v tickets remaining\n", remainingTickets)
+		remainingTickets -= userTickets
 
-	bookings = append(bookings, firstName+" "+lastName)
+		fmt.Printf("Thank you %v %v for booking %v tickets for %v. You will receive a convirmation email at %v\n", firstName, lastName, userTickets, conferenceName, email)
 
-	fmt.Printf("Data type : %T\n", bookings)
-	fmt.Printf("Data : %v\n", bookings)
-	fmt.Printf("Data length : %v\n", len(bookings))
+		fmt.Printf("There are %v tickets remaining\n", remainingTickets)
+
+		bookings = append(bookings, firstName+" "+lastName)
+
+		fmt.Printf("Data type : %T\n", bookings)
+		fmt.Printf("Data : %v\n", bookings)
+		fmt.Printf("Data length : %v\n", len(bookings))
+
+		// --- CONDITIONAL
+		if remainingTickets <= 0 {
+			fmt.Println("All tickets are sold out")
+			break
+		}
+
+	}
+
+	// --- CONTOH LAIN DARI LOOPING
+	kasus := 5
+	for i := 0; i < kasus; i++ {
+		fmt.Println("Kasus ke ", i)
+	}
+
+	books := []string{"Book 1", "Book 2", "Book 3"}
+	for _, book := range books {
+		fmt.Println(book)
+	}
 
 }
