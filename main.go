7bfeabf074bb2182c184091035f179cc80a7a54e
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2024-10-23 15:59:30 +0700

    feat: user input

--- main.go
+++ main.go
@@ -9,7 +9,26 @@ func main() {
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
 
-	fmt.Println("Welcome to %v bookin system", conferenceName)
-	fmt.Println("We have total of %v tickets and %v are tickets available", conferenceTickets, remainingTickets)
+	fmt.Printf("Welcome to %v bookin system\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are tickets available\n", conferenceTickets, remainingTickets)
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Please enter your first name")
+	fmt.Scanln(&firstName)
+
+	fmt.Println("Please enter your last name")
+	fmt.Scanln(&lastName)
+
+	fmt.Println("Please enter your email")
+	fmt.Scanln(&email)
+
+	fmt.Println("Please enter the number of tickets you want to book")
+	fmt.Scanln(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets for %v. You will receive a convirmation email at %v\n", firstName, lastName, userTickets, conferenceName, email)
 
 }
