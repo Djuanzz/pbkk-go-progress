d5c44ebd7cf14f7723add9fc1070e78682f08a1e
Adnan Abdullah Juan <adnanjuan06@gmail.com>
2024-10-23 15:53:35 +0700

    feat: data type

--- main.go
+++ main.go
@@ -4,4 +4,12 @@ import "fmt"
 
 func main() {
 	fmt.Println("PBKK GOLANG SECTION")
+
+	conferenceName := "PBKK"
+	const conferenceTickets = 50
+	var remainingTickets uint = 50
+
+	fmt.Println("Welcome to %v bookin system", conferenceName)
+	fmt.Println("We have total of %v tickets and %v are tickets available", conferenceTickets, remainingTickets)
+
 }
